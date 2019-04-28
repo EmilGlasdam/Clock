2e26a12e8bf546b5bfa398dccb0110f759eeabbb
Jacob Glasdam <jacob@medware.dk>
2019-04-28 20:46:02 +0200

    Seconds_hand

--- clock.rb
+++ clock.rb
@@ -1,11 +1,25 @@
 require 'ruby2d'
 require_relative 'face'
+require_relative 'hand'
 
 
 set(background: 'white', title: "Det analoge ur")
 
 face = Face.new(x: 320,y: 240, radius: 200)
+seconds_hand = Hand.new(x: 320, y: 240, length: 180)
 
 face.draw
+seconds_hand.draw
+
+angle = 0
+
+seconds_hand.angle = angle
+
+update do
+  angle += 1
+  angle = angle % 360
+  seconds_hand.angle = angle
+end
 
 show
+
---
+++ hand.rb
@@ -0,0 +1,34 @@
+
+class Hand
+
+  attr_accessor :x, :y, :length
+
+  def initialize(options = {})
+    self.x = options[:x]
+    self.y = options[:y]
+    self.length = options[:length]
+  end
+
+  def draw
+    line
+  end
+
+  def angle=(angle)
+    rad = ((angle-90)*Math::PI)/180.0
+    x_factor = Math.cos(rad)
+    y_factor = Math.sin(rad)
+    line.x2 = x + x_factor*length
+    line.y2 = y + y_factor*length
+  end
+
+  def line
+    @line ||= Line.new(
+      x1: x, y1: y,
+      x2: x, y2: y-length,
+      width: 2,
+      color: 'red',
+      z: 20
+    )
+  end
+
+end
