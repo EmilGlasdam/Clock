9fbf0d1e5b8b03fd5918a8bf30824ac3eb3fec63
Jacob Glasdam <jacob@medware.dk>
2019-05-05 09:46:21 +0200

    Sekunder på plads

--- clock.rb
+++ clock.rb
@@ -2,6 +2,9 @@ require 'ruby2d'
 require_relative 'face'
 require_relative 'hand'
 
+def set_seconds(tid)
+end
+
 set(background: 'white', title: "Det analoge ur")
 
 face = Face.new(x: 320,y: 240, radius: 200)
@@ -10,15 +13,13 @@ seconds_hand = Hand.new(x: 320, y: 240, length: 180)
 face.draw
 seconds_hand.draw
 
-angle = 0
-
-seconds_hand.angle = angle
-
 update do
-  angle += 1
-  angle = angle % 360
-  seconds_hand.angle = angle
+  tid = Time.now
+  sekunder = tid.sec
+  seconds_hand.angle = 6 * sekunder
 end
 
 show
+
+
 
